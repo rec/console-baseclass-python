5d7e657517c4b1c0c41d3b901e095f0b1934a27f
Vegard Sandengen <vladwow91@hotmail.com>
2012-12-17 01:14:30 +0100

    Added a defining structure with to lay the ground

---
+++ doc/source/index.rst
@@ -0,0 +1,16 @@
+.. console_baseclass_python documentation master file, created by
+   sphinx-quickstart on Sun Dec 16 22:12:03 2012.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Python Console Baseclass documentation
+======================================
+
+.. automodule:: console
+    :members:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
---
+++ src/console.py
@@ -0,0 +1,52 @@
+"""
+.. module:: console
+    :platform: Unix, Windows
+
+.. moduleauthor:: Vegard Sandengen <vegardsandengen@gmail.com>
+
+"""
+
+class Display_Information(object):
+    """This class is utilized to relay information flow throughout the program.
+
+    Can be routed through either logfile and/or stdout.
+    """
+
+    def __init__(self):
+        """
+        Args:
+            lol
+
+        Returns:
+            Nothing
+        """
+        pass
+
+    def verbose(self, format, *args):
+        """Outputs message to appropriate channels if enabled at initialization.
+
+
+        Args:
+            - format (string): Message to submit, with associated identifiers for argument inputs.
+            - *args (): Arguments will be supplied to identifiers in *format* in-order.
+                No sanity check is performed on relation with identifier and supplied argument.
+        """
+        pass
+
+class Command(object):
+    """Holds all information about an action, its available flags and associated methods.
+    """
+
+    def __init__(self):
+        """
+        Args:
+            lol
+
+        Returns:
+            Nothing
+
+        Raises:
+            Nothing
+        """
+
+
